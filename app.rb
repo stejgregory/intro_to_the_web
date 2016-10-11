0db1f4b82e0f26def77f57868fd6054372539643
michkles <michkles@gmail.com>
2016-10-11 11:07:19 +0100

    step12 finished

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/new' do
   "new new new"
 end
 
-get '/cat' do
+get '/random-cat' do
   @names = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @names = params[:name]
   erb :index
 end
---
+++ new.rb
@@ -0,0 +1,11 @@
+# arr = ['dssdds']
+#
+# puts arr
+# p arr
+
+def new(*arg)
+#  puts arg
+  puts arg.count
+end
+
+new(1, 2, 3, 4)
